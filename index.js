4b1c88f3a815aaab112aa1434c7e0de829618b51
Christopher Houghton <ChrisHoughton@ChristopherMBP.fios-router.home>
2021-09-12 21:13:23 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,64 @@
 // Your code here
+function mapToNegativize(sourceArray) {
+    let newArray = [];
+    for (let i = 0; i < sourceArray.length; i++) {
+        newArray.push(-sourceArray[i])
+    }
+    return newArray
+}
+
+function mapToNoChange(sourceArray) {
+    let newArray = [];
+    for (let i = 0; i < sourceArray.length; i++) {
+        newArray.push(sourceArray[i])
+    }
+    return newArray
+}
+
+function mapToDouble(sourceArray) {
+    let newArray = [];
+    for (let i = 0; i < sourceArray.length; i++) {
+        newArray.push(sourceArray[i] * 2)
+    }
+    return newArray
+}
+
+function mapToSquare(sourceArray) {
+    let newArray = [];
+    for (let i = 0; i < sourceArray.length; i++) {
+        newArray.push(sourceArray[i] * sourceArray[i])
+    }
+    return newArray
+}
+
+// filter like functions
+
+// -`reduceToTotal(sourceArray, startingPoint)`
+// - `reduceToAllTrue(sourceArray)`
+// - `reduceToAnyTrue(sourceArray)`
+
+function reduceToTotal(sourceArray, startingPoint = 0) {
+    let value = startingPoint;
+    for (let i = 0; i < sourceArray.length; i++) {
+        value += sourceArray[i];
+    }
+    return value
+}
+
+function reduceToAllTrue(sourceArray) {
+    let boolean;
+    for (let i = 0; i < sourceArray.length; i++) {
+        boolean = Boolean(sourceArray[i])
+        if(boolean === false) break;
+    }
+    return boolean
+}
+
+function reduceToAnyTrue(sourceArray) {
+    let boolean;
+    for (let i = 0; i < sourceArray.length; i++) {
+        boolean = !!sourceArray[i]
+        if(boolean === true) break;
+    }
+    return boolean
+}
